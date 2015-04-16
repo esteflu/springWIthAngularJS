fa8e93f04d6aee6a43d1407c625691ed34ffdae2
stefanlundberg <lundberg.stefan@gmail.com>
2015-04-16 20:44:05 +0200

    Removed old shit. Added controller and model

---
+++ src/main/java/app/controller/ValidationController.java
@@ -0,0 +1,28 @@
+package app.controller;
+
+import app.model.JSONResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by stefanlundberg on 15-04-16.
+ */
+
+@Controller
+@RequestMapping("/validate")
+public class ValidationController {
+
+    @RequestMapping(
+            value = "/{number}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE )
+
+    public ResponseEntity<JSONResponse> validatePhoneNumber(@PathVariable("number") String number) {
+        return new ResponseEntity<JSONResponse>(new JSONResponse(number), HttpStatus.OK);
+    }
+}
