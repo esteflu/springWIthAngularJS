61926aa201e2c902d849b3be9106edc24676d334
stefanlundberg <lundberg.stefan@gmail.com>
2015-04-21 20:45:11 +0200

    Formating.

--- src/main/webapp/app/services/validation/validation.js
+++ src/main/webapp/app/services/validation/validation.js
@@ -1,14 +1,17 @@
 var services = angular.module('services', ['ngResource']);
 
 services.factory('ValidationService', ['$resource', function ($resource) {
-       return $resource('/api/:method/:number', {
-           number:'@number'
-       }, {
-           validateNumber: {
-               method: 'GET',
-               params: {
-                   method: 'validate'
+       return $resource('/api/:method/:number',
+           {
+               number:'@number'
+           },
+           {
+               validateNumber: {
+                   method: 'GET',
+                   params: {
+                       method: 'validate'
+                   }
                }
            }
-       });
+       );
 }]);
